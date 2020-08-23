fb14d5012e3bd98f16f5f0d571f2d9ce12fc5c9e
gary <gr.candia93@gmail.com>
2020-08-22 19:20:34 -0500

    Hasta buscador

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import *
 from werkzeug.utils import secure_filename
-import pymysql.cursors
-import random
+from datetime import date, datetime
+import pymysql
+import time
 
 app = Flask(__name__)
 
@@ -11,12 +12,11 @@ app.config['UPLOAD_FOLDER'] = "./archivos"  # directorio de la carpeta donde est
 
 @app.route("/")
 def welcome():
-    return render_template('MenuPrincipal.html')
+    return redirect('MenuPrincipal.html')
 
 
 @app.route("/tabla")
 def contenidotabla():
-    # connection = pypyodbc.connect('Driver={MySQL ODBC 3.51 Driver};Server=.;Database=BD_PAGINA')  # Creating Cursor
     connection = pymysql.connect(host='localhost',
                                  user='root',
                                  password='linux321',
@@ -24,84 +24,136 @@ def contenidotabla():
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
     cursor = connection.cursor()
-    cursor.execute("SELECT RUTA FROM TESTDOC;")
-    s = ""
+    cursor.execute('SELECT * FROM DOCUMENTO;')
+    table = ""
     for row in cursor:
-        w = str(row)
-        s = s + w[2:-3] + "&nbsp;&nbsp<a href= " + w[2:-3] + "> Descargar</a> <br><br>"
+        table = table + row['TITULO_DOCUMENTO'] + '&nbsp;&nbsp;<a href=' + row['RUTA_DOCUMENTO'] + '>Descargar</a><br>'
     connection.close()
-    return "<html><body>" + s + "</body></html>"
+    return "<html><body>" + table + "</body></html>"
 
 
-@app.route("/SubirDocumento.html")
-def upload_file():
-    return render_template('SubirDocumento.html')
-
-
-@app.route("/SubirDocumento.html", methods=['POST'])
-def uploader():
+@app.route("/SubirDocumento.html", methods=['GET', 'POST'])
+def subir():
     if request.method == "POST":
-        f = request.files['archivo']
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file = request.files['archivo']
+        filename = secure_filename(file.filename)
+        TITULO = request.form['titulo']
+        FECHA = date.today()
+        IDIOMA = request.form['idioma']
+        ESTADO = 'esp'
 
-        # connection1 = pypyodbc.connect('Driver={MySQL ODBC 3.51 Driver};Server=.;Database=BD_PAGINA')  # Creating Cursor
-        connection1 = pymysql.connect(host='localhost',
-                                      user='root',
-                                      password='linux321',
-                                      db='BD_PAGINA',
-                                      charset='utf8mb4',
-                                      cursorclass=pymysql.cursors.DictCursor)
-        cursor1 = connection1.cursor()
-        CODDOC = str(random.randint(1, 100))
-        TITULO = "TESIS N* "+CODDOC
-        RUTA = "./archivos/"+str(f.filename)
-        query = "INSERT INTO TESTDOC VALUES ('"+CODDOC+"','"+TITULO+"','"+RUTA+"');"
-        cursor1.execute(query)
-        connection1.commit()
-        connection1.close()
+        CODIGO_USUARIO = session['codigo_usuario']
+        PALABRAS = request.form['palabras'].replace(' ', '').lower().split(',')
 
+        if TITULO != '' and filename[-4:] == '.pdf' and PALABRAS != '':
+            fechaHora = datetime.now()
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], str(fechaHora.strftime("%d-%m-%Y_%H-%M-%S-%f"))+"_"+str(filename)))
+
+            RUTA = "./archivos/"+str(fechaHora.strftime("%d-%m-%Y_%H-%M-%S-%f"))+"_"+str(filename)
+
+            connection = pymysql.connect(host='localhost',
+                                         user='root',
+                                         password='linux321',
+                                         db='BD_PAGINA',
+                                         charset='utf8mb4',
+                                         cursorclass=pymysql.cursors.DictCursor)
+            cursor = connection.cursor()
+
+            cursor.execute('INSERT INTO DOCUMENTO VALUES (NULL,%s,%s,%s,%s,%s,%s,%s);', (TITULO, FECHA, IDIOMA, ESTADO, RUTA, '-', CODIGO_USUARIO))
+            cursor.execute('SELECT COD_DOCUMENTO FROM DOCUMENTO ORDER BY COD_DOCUMENTO DESC LIMIT 1;')
+            cod_documento = cursor.fetchone()['COD_DOCUMENTO']
+            connection.commit()
+
+            for palabra in PALABRAS:
+                cursor.execute('SELECT * FROM PALABRAS_CLAVE WHERE PALABRA=%s;', palabra)
+                palabra_clave = cursor.fetchone()
+                if palabra_clave is None:
+                    cursor.execute('INSERT INTO PALABRAS_CLAVE VALUES (NULL,%s);', palabra)
+                    connection.commit()
+
+            for palabra in PALABRAS:
+                cursor.execute('SELECT COD_PALABRA FROM PALABRAS_CLAVE WHERE PALABRA =%s;', palabra)
+                cod_palabra = cursor.fetchone()['COD_PALABRA']
+                cursor.execute(
+                    'INSERT INTO DOCUMENTO_POR_PALABRA VALUES (%s,%s);', (cod_documento, cod_palabra))
+                connection.commit()
+
+            connection.close()
+
+            return render_template('MenuInicioUser.html', name=session['nombre_usuario'])
+
+        elif filename[-4:] != '.pdf':
+            return render_template('SubirDocumento.html', mensaje='Debe subir un documento pdf')
+
+        else:
+            return render_template('SubirDocumento.html', mensaje='Debe llenar todos los campos')
+
+    else:
         return render_template('SubirDocumento.html')
 
 
-@app.route("/Buscador.html")
+@app.route("/Buscador.html", methods=['GET', 'POST'])
 def buscar():
-    return render_template('Buscador.html')
+
+    if not session:
+        return render_template('MenuPrincipal.html')
+    else:
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='linux321',
+                                     db='BD_PAGINA',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        cursor = connection.cursor()
+
+        if request.method == 'POST':
+            textoBuscar = request.form['textoBuscar']
+            cursor.execute('SELECT * FROM DOCUMENTO WHERE COD_DOCUMENTO IN (SELECT DOCUMENTO_COD_DOCUMENTO FROM DOCUMENTO_POR_PALABRA WHERE PALABRAS_CLAVE_COD_PALABRA=(SELECT COD_PALABRA FROM PALABRAS_CLAVE WHERE PALABRA=%s) );', textoBuscar)
+            documentosEncontrados = cursor.fetchall()
+
+            return render_template('Buscador.html', documentos=documentosEncontrados)
+        else:
+            return render_template('Buscador.html')
 
 
 @app.route("/IniciarSesion.html", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
-        password = request.form['password'].encode("utf-8")
-        # connection2 = pypyodbc.connect('Driver={SQL Server};Server=.;Database=BD_PAGINA')  # Creating Cursor
-        connection2 = pymysql.connect(host='localhost',
-                                      user='root',
-                                      password='linux321',
-                                      db='BD_PAGINA',
-                                      charset='utf8mb4',
-                                      cursorclass=pymysql.cursors.DictCursor)
-        cursor2 = connection2.cursor()
-        query = "SELECT * FROM USUARIO WHERE CORREO='"+str(email)+"';"
-        cursor2.execute(query)
-        user = cursor2.fetchone()
+        password = request.form['password'].encode('utf-8')
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='linux321',
+                                     db='BD_PAGINA',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM USUARIO WHERE CORREO_USUARIO=%s;', str(email))
+        user = cursor.fetchone()
+
         if user is None:
-            connection2.close()
+            connection.close()
             return render_template('IniciarSesion.html')
-        # return "<html><body>"+ str(user[2]) +"     aaaaaaa"+password.decode("utf-8")+"</body></html>"
-        elif password.decode("utf-8") == str(user['PASSWORD']):
-            session['name'] = user['NOMBRE']
-            session['email'] = user['CORREO']
-            connection2.close()
-            return render_template('MenuInicioUser.html')
+        elif password.decode('utf-8') == user['PASSWORD_USUARIO']:
+            session['codigo_usuario'] = user['COD_USUARIO']
+            session['nombre_usuario'] = user['NOMBRE_USUARIO']
+            session['tipo_usuario'] = user['TIPO_USUARIO']
+            connection.close()
+
+            if session['tipo_usuario'] == 'adm':
+                return render_template('MenuInicioOrg.html', name=session['nombre_usuario'])
+            else:
+                return render_template('MenuInicioUser.html', name=session['nombre_usuario'])
+
+    else:
+
+        if not session:
+            return render_template('IniciarSesion.html')
         else:
-            return render_template('IniciarSesion.html')
-    return render_template('IniciarSesion.html')
-
-
-@app.route("/PublicarDocumento.html")
-def document():
-    return render_template('PublicarDocumento.html')
+            if session['tipo_usuario'] == 'adm':
+                return render_template('MenuInicioOrg.html', name=session['nombre_usuario'])
+            else:
+                return render_template('MenuInicioUser.html', name=session['nombre_usuario'])
 
 
 @app.route("/ValidarDocumento.html")
@@ -109,7 +161,7 @@ def validar():
     return render_template('ValidarDocumento.html')
 
 
-@app.route("/Documentos.html")
+@app.route("/Documentos.html")  # ver para que sirve
 def documentos():
     return render_template('Documentos.html')
 
@@ -119,19 +171,103 @@ def desplegar():
     return render_template('MenuPrincipal.html')
 
 
-@app.route("/MenuInicioOrg.html")
+@app.route("/MenuInicioOrg.html", methods=['GET'])
 def desplegar2():
-    return render_template('MenuInicioOrg.html')
+    if not session:
+        return render_template('MenuPrincipal.html')
+    else:
+        return render_template('MenuInicioOrg.html', name=session['nombre_usuario'])
 
 
-@app.route("/MenuInicioUser.html")
+@app.route("/MenuInicioUser.html", methods=['GET', 'POST'])
 def desplegar3():
-    return render_template('MenuInicioUser.html')
+
+    if not session:
+        return render_template('MenuPrincipal.html')
+    else:
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='linux321',
+                                     db='BD_PAGINA',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        cursor = connection.cursor()
+
+        if request.method == 'POST':
+            codBorrar = str(list(request.form.keys())[0])
+            cursor.execute('DELETE FROM DOCUMENTO WHERE COD_DOCUMENTO=%s;', codBorrar)
+            connection.commit()
+
+        cursor.execute('SELECT * FROM DOCUMENTO WHERE USUARIO_COD_USUARIO=%s', session['codigo_usuario'])
+        documentosSubidos = cursor.fetchall()
+
+        return render_template('MenuInicioUser.html', name=session['nombre_usuario'], documentos=documentosSubidos)
 
 
-@app.route("/MenuInicioUser/PublicarDocumento.html")
+@app.route("/PublicarDocumento.html", methods=['GET', 'POST'])
 def publicar():
-    return render_template('PublicarDocumento.html')
+    if request.method == "POST":
+        file = request.files['archivo']
+        filename = secure_filename(file.filename)
+        TITULO = request.form['titulo']
+        FECHA = date.today()
+        IDIOMA = request.form['idioma']
+        VALIDADO = 1
+
+        CODIGO_USUARIO = session['codigo_usuario']
+        PALABRAS = request.form['palabras'].split(',')
+
+        if TITULO != '' and filename[-4:] == '.pdf' and PALABRAS != '':
+            fechaHora = datetime.now()
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], str(fechaHora.strftime("%d-%m-%Y_%H-%M-%S-%f"))+"_"+str(filename)))
+
+            RUTA = "./archivos/"+str(fechaHora.strftime("%d-%m-%Y_%H-%M-%S-%f"))+"_"+str(filename)
+
+            connection = pymysql.connect(host='localhost',
+                                         user='root',
+                                         password='linux321',
+                                         db='BD_PAGINA',
+                                         charset='utf8mb4',
+                                         cursorclass=pymysql.cursors.DictCursor)
+            cursor = connection.cursor()
+
+            cursor.execute('INSERT INTO DOCUMENTO VALUES (NULL,%s,%s,%s,%s,%s,%s);', (TITULO, RUTA, FECHA, IDIOMA, VALIDADO, CODIGO_USUARIO))
+            cursor.execute('SELECT COD_DOCUMENTO FROM DOCUMENTO ORDER BY COD_DOCUMENTO DESC LIMIT 1;')
+            cod_documento = cursor.fetchone()['COD_DOCUMENTO']
+            connection.commit()
+
+            for palabra in PALABRAS:
+                cursor.execute('SELECT * FROM PALABRAS_CLAVE WHERE PALABRA=%s;', palabra)
+                palabra_clave = cursor.fetchone()
+                if palabra_clave is None:
+                    cursor.execute('INSERT INTO PALABRAS_CLAVE VALUES (NULL,%s);', (palabra))
+                    connection.commit()
+
+            for palabra in PALABRAS:
+                cursor.execute('SELECT COD_PALABRA FROM PALABRAS_CLAVE WHERE PALABRA =%s;', palabra)
+                cod_palabra = cursor.fetchone()['COD_PALABRA']
+                cursor.execute(
+                    'INSERT INTO DOCUMENTO_POR_PALABRA VALUES (%s,%s);', (cod_documento, cod_palabra))
+                connection.commit()
+
+            connection.close()
+
+            return render_template('MenuInicioOrg.html', name=session['nombre_usuario'])
+
+        elif filename[-4:] != '.pdf':
+            return render_template('PublicarDocumento.html', mensaje='Debe subir un documento pdf')
+
+        else:
+            return render_template('PublicarDocumento.html', mensaje='Debe llenar todos los campos')
+
+    else:
+        return render_template('PublicarDocumento.html')
+
+
+@app.route("/CerrarSesion.html", methods=['GET'])
+def cerrarSesion():
+    session.clear()
+    return render_template('MenuPrincipal.html')
 
 
 if __name__ == '__main__':
